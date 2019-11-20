57bb48b65e7de68951f6444b5ac89a8f45c603d9
Chris Zuber <shgysk8zer0@gmail.com>
2019-11-20 10:08:02 -0800

    Add basic map

--- js/index.js
+++ js/index.js
@@ -2,11 +2,13 @@ import 'https://cdn.kernvalley.us/js/std-js/deprefixer.js';
 import 'https://cdn.kernvalley.us/js/std-js/shims.js';
 import 'https://cdn.kernvalley.us/components/share-button.js';
 import 'https://cdn.kernvalley.us/components/current-year.js';
-import 'https://cdn.kernvalley.us/components/bacon-ipsum.js';
 import 'https://cdn.kernvalley.us/components/gravatar-img.js';
 import 'https://cdn.kernvalley.us/components/login-button.js';
 import 'https://cdn.kernvalley.us/components/logout-button.js';
-import {$, ready, registerServiceWorker} from 'https://cdn.kernvalley.us/js/std-js/functions.js';
+import HTMLOpenStreetMapElement from 'https://cdn.kernvalley.us/components/open-street-map.js';
+import { $, ready, registerServiceWorker } from 'https://cdn.kernvalley.us/js/std-js/functions.js';
+
+customElements.define(HTMLOpenStreetMapElement.tagName, HTMLOpenStreetMapElement);
 
 if (document.documentElement.dataset.hasOwnProperty('serviceWorker')) {
 	registerServiceWorker(document.documentElement.dataset.serviceWorker).catch(console.error);
